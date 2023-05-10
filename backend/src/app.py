44766a6a475f2228baf83f0e070442a1a4d8c1b8
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-05-10 17:04:25 -0600

    editar, eliminar, agregar curso

--- backend/src/app.py
+++ backend/src/app.py
@@ -134,12 +134,12 @@ def editar_maestro(id):
         # Obtener los datos del trabajador por su ID y mostrar el formulario de edición
         with connection.cursor() as cursor:
             cursor.execute("SELECT * FROM maestro WHERE id_maestro = %s", (id,))
-            worker = cursor.fetchone()
+            maestro = cursor.fetchone()
 
-        if worker:
-            return render_template('editar_maestro.html', worker=worker)
+        if maestro:
+            return render_template('editar_maestro.html', maestro=maestro)
         else:
-            return 'Trabajador no encontrado'
+            return 'Maestro no encontrado'
 
     elif request.method == 'POST':
         # Actualizar los datos del trabajador en la base de datos
@@ -172,10 +172,10 @@ def eliminar_maestro(id):
             flash('El maestro ha sido eliminado exitosamente.', 'success')
             return redirect('/maestros3')
 
-        return render_template('eliminar_maestro.html', worker_id=id)
+        return render_template('eliminar_maestro.html', maestro_id=id)
 
     except Exception as ex:
-        flash('Ocurrió un error al intentar eliminar el trabajador.', 'error')
+        flash('Ocurrió un error al intentar eliminar el maestro.', 'error')
         return redirect('/maestros3')
 
 @app.route('/maestros3')
@@ -191,6 +191,81 @@ def mestros3():
 @app.route('/cursos')
 def cursos():
     return render_template('cursos.html')
+
+@app.route('/cursos2', methods=['POST'])
+def cursos2():
+    try:
+        id_curso = request.form['id_curso']
+        nombre = request.form['nombre']
+        salon = request.form['salon']
+        hora_inicio = request.form['hora_inicio']
+        hora_fin = request.form['hora_fin']
+
+        with connection.cursor() as cursor:
+            cursor.execute("""INSERT INTO curso VALUES (%s, %s, %s, %s, %s)""", 
+                           (id_curso, nombre, salon, hora_inicio, hora_fin))
+            connection.commit()  
+        return redirect('/cursos3')
+    except Exception as ex:
+        return render_template('cursos.html')
+
+@app.route('/cursos2/<id>/edit', methods=['GET', 'POST'])
+def editar_curso(id):
+    if request.method == 'GET':
+        # Obtener los datos del trabajador por su ID y mostrar el formulario de edición
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM curso WHERE id_curso = %s", (id,))
+            cur = cursor.fetchone()
+
+        if cur:
+            return render_template('editar_curso.html', cur=cur)
+        else:
+            return 'Curso no encontrado'
+
+    elif request.method == 'POST':
+        # Actualizar los datos del trabajador en la base de datos
+        id_curso = request.form['id_curso']
+        nombre = request.form['nombre']
+        salon = request.form['salon']
+        hora_inicio = request.form['hora_inicio']
+        hora_final = request.form['hora_fin']
+
+        with connection.cursor() as cursor:
+            cursor.execute("""UPDATE curso SET
+                nombre = %s,
+                salon = %s,
+                hora_inicio = %s,
+                hora_fin = %s
+                WHERE id_curso = %s
+            """, (nombre, salon, hora_inicio, hora_final, id_curso))
+            connection.commit()
+
+        return redirect('/cursos3')
+    
+@app.route('/cursos2/<id>/delete', methods=['GET', 'POST'])
+def eliminar_curso(id):
+    try:
+        if request.method == 'POST':
+            with connection.cursor() as cursor:
+                cursor.execute("DELETE FROM curso WHERE id_curso = %s", (id,))
+                connection.commit()
+
+            flash('El curso ha sido eliminado exitosamente.', 'success')
+            return redirect('/cursos3')
+
+        return render_template('eliminar_curso.html', curso_id=id)
+
+    except Exception as ex:
+        flash('Ocurrió un error al intentar eliminar el curso.', 'error')
+        return redirect('/cursos3')
+
+@app.route('/cursos3')
+def cursos3():
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT * FROM curso
+                        ORDER BY id_curso ASC""")
+        rows = cursor.fetchall()
+        return render_template('cursos3.html', rows=rows)
 
 def page_not_found(error):
     return "<h1>Not found page</h1>", 404
