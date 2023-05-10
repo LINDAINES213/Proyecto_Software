5b0834cb8e7c1369200d27b84e7e461b2f6d5000
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-05-09 18:25:57 -0600

    edit y delete terminados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,11 @@ const baseUrl = "http://127.0.0.1:5000"
 function App() {
 
   const [description, setDescription] = useState("")
+  const[editDescription, setEditDescription] = useState("")
   const[eventsList, setEventsList] = useState([])
+  const[eventId, setEventId] = useState(null)
 
-  const fetchEvents = async (e) =>{
-    e.preventDefault()
+  const fetchEvents = async () =>{
     try{
       const data = await axios.get(`${baseUrl}/events`, {description})
       setEventsList([...eventsList, data.data])
@@ -21,17 +22,57 @@ function App() {
       const { events } = data.data
       setEventsList(events)
     } catch(err){
+      console.log(err.message)
+    }
+    
+  }
+
+  const handleChange = (e, field) => {
+    if (field === 'edit'){
+      setEditDescription(e.target.value)
+    } else{
+      setDescription(e.target.value)
+    }
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try{
+      if (editDescription){
+        const data = await axios.put(`${baseUrl}/events/${eventId}`, {description: editDescription}) 
+        const updatedEvent = data.data.event
+        const updatedList = eventsList.map(event => {
+          if (event.id === eventId){
+            return event = updatedEvent
+          }
+          return event
+        })
+        setEventsList(updatedList)
+      } else{
+        const data = await axios.post(`${baseUrl}/events`, {description})
+        setEventsList([...eventsList, data.data])
+      }
+      setDescription('')
+      setEditDescription('')
+      setEventId(null)
+    } catch(err){
+      console.log(err.message)
+    }
+  }
+
+  const handleDelete = async (id) =>{
+    try{
+      await axios.delete(`${baseUrl}/events/${id}`)
+      const updatedList = eventsList.filter(event => event.id != id)
+      setEventsList(updatedList)
+    } catch(err){
       console.error(err.message)
     }
   }
 
-  const handleChange = e => {
-    setDescription(e.target.value)
-  }
-
-  const handleSubmit = e =>{
-    e.preventDefault()
-    console.log(description)
+  const toggleEdit = (event) => {
+    setEventId(event.id)
+    setEditDescription(event.description)
   }
 
   useEffect(() => {
@@ -43,17 +84,31 @@ function App() {
         <section>
           <form onSubmit={handleSubmit}>
             <label htmlFor="description">Description</label>
-            <input onChange={handleChange} type="text" name="description" id="description" value={description}/>
+            <input onChange={(e) => handleChange(e, 'description')} type="text" name="description" id="description" placeholder='Describe the event' value={description}/>
             <button type="submit">Submit</button>
           </form>
         </section>
         <section>
-          <ul>
-            {eventsList.map(event => {
+        <ul>
+          {eventsList.map(event => {
+            if (eventId === event.id){
               return(
-                <li key={event.id}>{event.description}</li>
+                <form onSubmit={handleSubmit} key={event.id}>                  
+                  <input onChange={(e) => handleChange(e, 'edit')} type="text" name="editDescription" id="editDescription" value={editDescription} />
+                  <button type='submit'>Submit</button>
+                </form>
               )
-            })}
+            } else{
+              return(
+                  <li style={{display: "flex"}} key={event.id}>
+                    {format(new Date(event.created_at), "MM/dd, p")}: {" "}
+                    {event.description}
+                    <button onClick={() => toggleEdit(event)}>Edit</button>
+                    <button onClick={() => handleDelete(event.id)}>x</button>
+                  </li>
+              )
+            }
+          })}
           </ul>
         </section>
       </div>
