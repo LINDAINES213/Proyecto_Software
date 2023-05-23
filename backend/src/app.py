991bf30c67976fb4f21929d1539a602a9360e8d5
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-05-23 13:39:07 -0600

    modificaciones maestros

--- backend/src/app.py
+++ backend/src/app.py
@@ -115,26 +115,28 @@ def maestros():
 @app.route('/maestros2', methods=['POST'])
 def mestros2():
     try:
-        id_maestro = request.form['id_maestro']
+        dpi = request.form['dpi']
         nombres = request.form['nombres']
         apellidos = request.form['apellidos']
-        curso = request.form['curso']
+        curso_1 = request.form['curso_1']
+        curso_2 = request.form['curso_2']
         salario = request.form['salario']
+        fecha_contratacion = request.form['fecha_contratacion']
 
         with connection.cursor() as cursor:
-            cursor.execute("""INSERT INTO maestro VALUES (%s, %s, %s, %s, %s)""", 
-                           (id_maestro, nombres, apellidos, curso, salario))
+            cursor.execute("""INSERT INTO maestros VALUES (%s, %s, %s, %s, %s, %s, %s)""", 
+                           (dpi, nombres, apellidos, curso_1, curso_2, salario, fecha_contratacion))
             connection.commit()  
         return redirect('/maestros3')
     except Exception as ex:
         return render_template('maestros.html')
 
-@app.route('/maestros2/<id>/edit', methods=['GET', 'POST'])
-def editar_maestro(id):
+@app.route('/maestros2/<dpi>/edit', methods=['GET', 'POST'])
+def editar_maestro(dpi):
     if request.method == 'GET':
         # Obtener los datos del trabajador por su ID y mostrar el formulario de edición
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM maestro WHERE id_maestro = %s", (id,))
+            cursor.execute("SELECT * FROM maestros WHERE dpi = %s", (dpi,))
             maestro = cursor.fetchone()
 
         if maestro:
@@ -144,36 +146,38 @@ def editar_maestro(id):
 
     elif request.method == 'POST':
         # Actualizar los datos del trabajador en la base de datos
-        id_maestro = request.form['id_maestro']
+        dpi = request.form['dpi']
         nombres = request.form['nombres']
         apellidos = request.form['apellidos']
-        curso = request.form['curso']
+        curso_1 = request.form['curso_1']
+        curso_2 = request.form['curso_2']
         salario = request.form['salario']
 
         with connection.cursor() as cursor:
-            cursor.execute("""UPDATE maestro SET
+            cursor.execute("""UPDATE maestros SET
                 nombres = %s,
                 apellidos = %s,
-                curso = %s,
+                curso_1 = %s,
+                curso_2 = %s,
                 salario = %s
-                WHERE id_maestro = %s
-            """, (nombres, apellidos, curso, salario, id_maestro))
+                WHERE dpi = %s
+            """, (nombres, apellidos, curso_1, curso_2, salario, dpi))
             connection.commit()
 
         return redirect('/maestros3')
     
-@app.route('/maestros2/<id>/delete', methods=['GET', 'POST'])
-def eliminar_maestro(id):
+@app.route('/maestros2/<dpi>/delete', methods=['GET', 'POST'])
+def eliminar_maestro(dpi):
     try:
         if request.method == 'POST':
             with connection.cursor() as cursor:
-                cursor.execute("DELETE FROM maestro WHERE id_maestro = %s", (id,))
+                cursor.execute("DELETE FROM maestros WHERE dpi = %s", (dpi,))
                 connection.commit()
 
             flash('El maestro ha sido eliminado exitosamente.', 'success')
             return redirect('/maestros3')
 
-        return render_template('eliminar_maestro.html', maestro_id=id)
+        return render_template('eliminar_maestro.html', maestro_dpi=dpi)
 
     except Exception as ex:
         flash('Ocurrió un error al intentar eliminar el maestro.', 'error')
@@ -182,8 +186,8 @@ def eliminar_maestro(id):
 @app.route('/maestros3')
 def mestros3():
     with connection.cursor() as cursor:
-        cursor.execute("""SELECT * FROM maestro
-                        ORDER BY id_maestro ASC""")
+        cursor.execute("""SELECT * FROM maestros
+                        ORDER BY dpi ASC""")
         rows = cursor.fetchall()
         return render_template('maestros3.html', rows=rows)
 
