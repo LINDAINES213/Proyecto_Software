e706919c5521f66c2135786b98afbac052751c1f
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-06-29 00:19:00 -0600

    plantilla formularios y tablas

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ class DevelopmentConfig(Config):
     DEBUG = True
     PGSQL_HOST = 'localhost'
     PGSQL_USER = 'postgres'
-    PGSQL_PASSWORD = 'Lind@1155'
-    PGSQL_DATABASE = 'proyecto_software2'
+    PGSQL_PASSWORD = 'Lind@115513'
+    PGSQL_DATABASE = 'proyecto_software'
 
 config = {
     'development': DevelopmentConfig
