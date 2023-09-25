1f801b029893d4f7cf0a636a49fd07a2399eb34b
Mario Guerra <maguerramorales@gmail.com>
2023-09-25 00:09:47 -0600

    Arreglo de calificaciones para director

--- routes/perfil_director/calificaciones.py
+++ routes/perfil_director/calificaciones.py
@@ -10,9 +10,8 @@ calificaciones_bp = Blueprint('calificaciones_blueprint', __name__)
 @login_required
 def calificaciones():
     with connection.cursor() as cursor:
-        cursor.execute("""SELECT * FROM estudiantes""")
-        '''cursor.execute("""SELECT id_estudiante, nombres, apellidos, grado, seccion,
-                        curso1, curso2, curso3, curso4, curso5, curso6, curso7, curso8, curso9, curso10, promedio FROM calificaciones
-                        ORDER BY grado ASC""")'''
+        cursor.execute("""SELECT id_estudiante, nombres, apellidos, grado, seccion,
+                        notaCurso1, notaCurso2, notaCurso3, notaCurso4, notaCurso5, notaCurso6, notaCurso7, notaCurso8, notaCurso9, notaCurso10, promedio FROM calificaciones
+                        ORDER BY grado ASC""")
         rows = cursor.fetchall()
         return render_template('director/calificaciones3.html', rows=rows)
