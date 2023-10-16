46e466c94b7978cbc1b9f1742ae073f979b51d4d
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-10-15 23:21:44 -0600

    edicion frontend trabajadores

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager, login_required, login_user, logout_user
 from flask_cors import CORS
 from dotenv import load_dotenv
 
+
 ################## Funcionalidades para Coordinador ###################
 from models.entities.User import User
 from models.entities.UserS import UserS
