f353d22f4c2e95e5b31afeb2842ded82cabac097
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-11-20 22:01:48 -0600

    edicion frontend salarios

--- static/js/admin/salarios.js
+++ static/js/admin/salarios.js
@@ -1,20 +1,31 @@
 window.onload = function() {
       // Iterar sobre todas las filas de la tabla
-      var table = document.getElementsByTagName("table")[0];
-      var rows = table.getElementsByTagName("tr");
-      
-      for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        
-        // Obtener el índice de la fila
-        var rowIndex = row.rowIndex;
-        
-        // Verificar si la fila está pintada en el almacenamiento local
-        if (localStorage.getItem("filaPintada_" + rowIndex) === "pagado") {
-          // Cambiar el color de fondo de la fila a verde claro
-          row.style.backgroundColor = "#B9E6C1";
-        }
+  var table = document.getElementsByTagName("table")[0];
+  var rows = table.getElementsByTagName("tr");
+
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+
+    // Obtener el índice de la fila
+    var rowIndex = row.rowIndex;
+
+    // Verificar si la fecha de pago existe en el almacenamiento local
+    var fechaPago = localStorage.getItem("fechaPago_" + rowIndex);
+
+    if (fechaPago) {
+      // Calcular el tiempo transcurrido en milisegundos
+      var hoy = Date.now();
+      var tiempoTranscurrido = hoy - parseInt(fechaPago, 10);
+
+      // Si ha pasado más de 35 días, quitar el color verde
+      if (tiempoTranscurrido > 35 * 24 * 60 * 60 * 1000) { // 35 días en milisegundos
+        revertirFila(rowIndex);
+      } else {
+        // Cambiar el color de fondo de la fila a verde claro
+        row.style.backgroundColor = "#B9E6C1";
       }
+    }
+  }
 };
 
 // Función para descargar la tabla en formato CSV
